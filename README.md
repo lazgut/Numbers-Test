86200430c21172f4e35cc17d5a652d043052e377
lazgut <artur.gayfulin@gmail.com>
2022-07-01 18:25:40 +0300

    Ready

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Numbers-Test
 Тестовое задание на developer
+Для запуска достаточно запустить main.py
+Таблица - https://docs.google.com/spreadsheets/d/1inELIBAkvAozqu8oSu_6O9vACC9LpcR6k8RT5pzTKYA/edit#gid=1777950449
+Установить на venv:
+pip install google-api-python-client oauth2client
+pip install pandas
+pip install xmltodict
+pp install sqlalchemy
+pip install psycopg2
+
+для PostgreSQL - config.py
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from time import sleep
 
 import httplib2
@@ -16,7 +14,7 @@ import psycopg2
 from config import host, port, user, password, db_name
 from sqlalchemy import create_engine
 
-
+# Подключаемся к моей гугл таблице
 CREDENTIALS_FILE = 'creds.json'
 spreadsheet_id = '1inELIBAkvAozqu8oSu_6O9vACC9LpcR6k8RT5pzTKYA'
 
@@ -30,11 +28,11 @@ service = discovery.build('sheets', 'v4', http=httpAuth)
 source_spreadsheet_id = '1LTejK-Oo7L1bFreBIIcEZnF1W1RCC1s_jos3EuIP0jI'
 
 
-def spreadsheet_id_append(source_spreadsheet_id: str):
+def spreadsheet_id_append(source_spreadsheet_id: str):  # Читаем таблицу из тестового задания, добавляем в неё нужную колонку и возвращаем DataFrame для дальнейшей работы
     values = service.spreadsheets().values().get(
         spreadsheetId=source_spreadsheet_id,
         majorDimension='ROWS',
-        range='Лист4'
+        range='Лист1'
     ).execute()
     values = values['values']
     df_values = pd.DataFrame(values[1:], columns=values[0])
@@ -44,7 +42,7 @@ def spreadsheet_id_append(source_spreadsheet_id: str):
     return df_values
 
 
-def spreadsheet_id_update(spreadsheet_id):
+def spreadsheet_id_update(source_spreadsheet_id, spreadsheet_id):  # Перезаписываем мою таблицу, использую DataFrame
     service.spreadsheets().values().batchUpdate(
         spreadsheetId=spreadsheet_id,
         body={
@@ -52,28 +50,28 @@ def spreadsheet_id_update(spreadsheet_id):
             "data": [
                 {"range": "Лист1",
                  "majorDimension": "ROWS",
-                 "values": [item for item in [spreadsheet_id_append(spreadsheet_id).columns.tolist()] + spreadsheet_id_append(spreadsheet_id).values.tolist()]
+                 "values": [item for item in [spreadsheet_id_append(source_spreadsheet_id).columns.tolist()] + spreadsheet_id_append(source_spreadsheet_id).values.tolist()]
                  }
             ]
         }
     ).execute()
 
 
-def dollar_ruble(date: list) -> float:
+def dollar_ruble(date: list) -> float:  # Возвращаем курс доллара в определенную дату
     url_dollar = "https://www.cbr.ru/scripts/XML_daily.asp?date_req={dd}/{mm}/{gg}"
     file_xml = urllib.request.urlopen(url_dollar.format(dd=date[0], mm=date[1], gg=date[2])).read()
     rubles = xmltodict.parse(file_xml)
     return float(rubles['ValCurs']['Valute'][10]['Value'].replace(',', '.'))
 
 
-def main():
+def main():  # Моя основная логика программы
     while True:
-        spreadsheet_id_update(spreadsheet_id)
-        DataFrame_to_PostgreSQL(spreadsheet_id_append(spreadsheet_id))
+        spreadsheet_id_update(source_spreadsheet_id, spreadsheet_id)
+        dataframe_to_postgresql(spreadsheet_id_append(spreadsheet_id))
         sleep(1)
 
 
-def DataFrame_to_PostgreSQL(df_values):
+def dataframe_to_postgresql(df_values):  # Записываем DataFrame в таблицу postgresql
     try:
         connection = psycopg2.connect(
             host=host,
